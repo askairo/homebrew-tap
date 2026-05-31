2a27c62ab3d85e423fd0338add6e0e29b086a3a3
dingsheng <dingsheng.xia@speedship.cn>
2026-05-31 20:40:53 +0800

    feat: switch clicky to cask distribution

---
+++ Casks/clicky.rb
@@ -0,0 +1,11 @@
+cask "clicky" do
+  version "0.1.3"
+  sha256 "01ea4c7998bb9eb78b31a66d4bfda2412bc732bd04b7387dc3e588a23eff7696"
+
+  url "https://github.com/askairo/Clicky/releases/download/v0.1.3/Clicky_0.1.3_aarch64.dmg"
+  name "Clicky"
+  desc "Cross-platform environment profile switcher for Spring Boot"
+  homepage "https://github.com/askairo/Clicky"
+
+  app "Clicky.app"
+end
